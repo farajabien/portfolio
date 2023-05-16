b31b31741601bc7f8ed023f01515afbc12b46309
Bienvenu <faraja.bien@gmail.com>
2023-05-16 16:56:35 +0300

    dropdown

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -6,6 +6,15 @@ import Link from "next/link"
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -21,51 +30,46 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex items-center justify-between gap-6 md:gap-10">
       <div className="flex items-center space-x-2">
-        <button className="block md:hidden" onClick={handleMenuToggle}>
-          <svg
-            className="h-6 w-6 fill-current text-gray-600"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            {menuOpen ? (
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"
-              />
-            ) : (
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"
-              />
-            )}
-          </svg>
-        </button>
-        <Link href="/" className="hidden items-center space-x-2 md:flex">
-          <span className="hidden font-bold sm:inline-block">
-            {siteConfig.name}
-          </span>
-        </Link>
-      </div>
+        <Menubar className="block md:hidden">
+          <MenubarMenu>
+            <MenubarTrigger>
+              <button onClick={handleMenuToggle}>
+                <svg
+                  className="h-6 w-6 fill-current"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"
+                  />
+                </svg>
+              </button>
+            </MenubarTrigger>
+            <MenubarContent>
+              {items?.map(
+                (item, index) =>
+                  item.href && (
+                    <Link href={item.href}>
+                      <MenubarItem key={index}>{item.title}</MenubarItem>
+                    </Link>
+                  )
+              )}
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
 
-      <div
-        className={cn("md:flex md:flex-row md:space-x-4", {
-          hidden: !menuOpen || !items || items.length === 0,
-        })}
-      >
+        <Link href="/" className=" items-center space-x-2 md:flex">
+          <span className="font-bold sm:inline-block">{siteConfig.name}</span>
+        </Link>
         {items?.map(
           (item, index) =>
             item.href && (
               <Link
-                key={index}
                 href={item.href}
-                className="
-                  px-4
-                  py-2
-                  text-sm
-                  font-medium
-                "
+                key={index}
+                className={cn("hidden items-center space-x-2 md:flex")}
               >
                 {item.title}
               </Link>
