43b9b94e1b4ba903fc0e65c9919847c7ea355c41
Bienvenu <faraja.bien@gmail.com>
2023-05-18 23:05:42 +0300

    added commission

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import {
   FaMobileAlt,
   FaNodeJs,
   FaPencilAlt,
+  FaPercent,
   FaReact,
   FaRegFilePdf,
   FaSearch,
@@ -187,10 +188,19 @@ export default function IndexPage() {
 
         <div className="mt-12">
           <div className="flex items-center space-x-2">
-            <FaDollarSign className="text-2xl text-yellow-500" />
-            <span className="text-xl font-semibold">
-              Pricing starts as low as $100 USD!
-            </span>
+            <div className="mt-12 rounded-lg bg-yellow-200 dark:bg-yellow-600 px-6 py-4">
+              <div className="flex items-center">
+                <FaDollarSign className="text-2xl text-yellow-500 dark:text-white" />
+                <span className="ml-2 text-xl font-semibold">
+                  Pricing starts as low as $100 USD!
+                </span>
+                <span className="ml-2 text-xl font-semibold">
+                  Bring a client and get a{" "}
+                  <FaPercent className="inline-block" />
+                  10 commission!
+                </span>
+              </div>
+            </div>
           </div>
         </div>
         <div className="mt-12">
